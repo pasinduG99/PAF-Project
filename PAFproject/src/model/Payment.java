16ab506c50d778e1fb0b04586ff870f86f86a92c
pasinduG99 <it20223380@my.sliit.lk>
2022-04-11 23:04:55 +0530

    Created paymemt model class

---
+++ PAFproject/src/model/Payment.java
@@ -0,0 +1,159 @@
+package model;
+
+import java.sql.*;
+
+public class Payment {
+	
+	private Connection connect()
+	{
+	Connection con = null;
+	try
+	{
+	Class.forName("com.mysql.jdbc.Driver");
+	//Provide the correct details: DBServer/DBName, username, password
+	con = DriverManager.getConnection("jdbc:mysql://localhost:3306/paymentdb",
+			"root", "Casio123");
+	}
+	catch (Exception e)
+	{e.printStackTrace();}
+	return con;
+	}
+	public String insertItem(String name, String nic, String description, String amount)
+	{
+	String output = "";
+	try
+	{
+	Connection con = connect();
+	if (con == null)
+	{return "Error while connecting to the database for inserting."; }
+	// create a prepared statement
+	String query = " insert into payments(`invoiceNo`,`name`,`NIC`,`description`,`amount`)" 
+	+ " values (?, ?, ?, ?, ?)";
+	PreparedStatement preparedStmt = con.prepareStatement(query);
+	// binding values
+	preparedStmt.setInt(1, 0);
+	preparedStmt.setString(2, name);
+	preparedStmt.setString(3, nic);
+	preparedStmt.setString(4, description);
+	preparedStmt.setDouble(5, Double.parseDouble(amount));
+	
+	
+	// execute the statement
+	
+	preparedStmt.execute();
+	con.close();
+	output = "Inserted successfully";
+	}
+	catch (Exception e)
+	{
+	output = "Error while inserting the payment.";
+	System.err.println(e.getMessage());
+	}
+	return output;
+	}
+	public String readPayments()
+	{
+	String output = "";
+	try
+	{
+	Connection con = connect();
+	if (con == null)
+	{return "Error while connecting to the database for reading."; }
+	// Prepare the html table to be displayed
+	output = "<table border='1'><tr><th>Name</th><th>NIC</th>" +
+	"<th>Description</th>" +
+	"<th>Amount</th>" +
+	"<th>Update</th><th>Remove</th></tr>";
+	String query = "select * from payments";
+	Statement stmt = con.createStatement();
+	ResultSet rs = stmt.executeQuery(query);
+	// iterate through the rows in the result set
+	while (rs.next())
+	{
+	String invoiceNo = Integer.toString(rs.getInt("invoiceNo"));
+	String name = rs.getString("name");
+	String NIC = rs.getString("NIC");
+	String description = rs.getString("description");
+	String amount = Double.toString(rs.getDouble("amount"));
+	
+	// Add into the html table
+	output += "<tr><td>" + invoiceNo + "</td>";
+	output += "<td>" + name + "</td>";
+	output += "<td>" + NIC + "</td>";
+	output += "<td>" + description + "</td>";
+	output += "<td>" + amount + "</td>";
+	// buttons
+	output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"
+	+ "<td><form method='post' action='items.jsp'>"
+	+ "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"
+	+ "<input name='invoiceNo' type='hidden' value='" + invoiceNo
+	+ "'>" + "</form></td></tr>";
+	}
+	con.close();
+	// Complete the html table
+	output += "</table>";
+	}
+	catch (Exception e)
+	{
+	output = "Error while reading the payments.";
+	System.err.println(e.getMessage());
+	}
+	return output;
+	}
+	public String updateItem(String invoiceNo ,String name, String nic, String description, String amount)
+	
+	{
+		String output = "";
+		try
+		{
+		Connection con = connect();
+		if (con == null)
+		{return "Error while connecting to the database for updating."; }
+		// create a prepared statement
+		String query = "UPDATE payments SET name=?,NIC=?,description=?,amount=? WHERE invoiceNo=?";
+		PreparedStatement preparedStmt = con.prepareStatement(query);
+		// binding values
+		preparedStmt.setString(1, name);
+		preparedStmt.setString(2, nic);
+		preparedStmt.setString(3, description);
+		preparedStmt.setDouble(4, Double.parseDouble(amount));
+		preparedStmt.setInt(5, Integer.parseInt(invoiceNo));
+		// execute the statement
+		preparedStmt.execute();
+		con.close();
+		output = "Updated successfully";
+		}
+		catch (Exception e)
+		{
+		output = "Error while updating the item.";
+		System.err.println(e.getMessage());
+		}
+		return output;
+		}
+		public String deletePayment(String invoiceNo)
+		{
+		String output = "";
+		try
+		{
+		Connection con = connect();
+		if (con == null)
+		{return "Error while connecting to the database for deleting."; }
+		// create a prepared statement
+		String query = "delete from items where invoiceNo=?";
+		PreparedStatement preparedStmt = con.prepareStatement(query);
+		// binding values
+		preparedStmt.setInt(1, Integer.parseInt(invoiceNo));
+		// execute the statement
+		preparedStmt.execute();
+		con.close();
+		output = "Deleted successfully";
+		}
+		catch (Exception e)
+		{
+		output = "Error while deleting the payment.";
+		System.err.println(e.getMessage());
+		}
+		return output;
+		}
+		
+}
